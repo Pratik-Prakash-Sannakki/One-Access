3d64cabeb902bc66e9229c018f5e10222b800272
Pratik P Sannakki <114252357+Pratik-Prakash-Sannakki@users.noreply.github.com>
2022-12-14 10:19:25 -0500

    Update sms.java

--- OneAccessApplication/src/UI/HospitalAdminRole/sms.java
+++ OneAccessApplication/src/UI/HospitalAdminRole/sms.java
@@ -7,14 +7,14 @@ import com.twilio.type.PhoneNumber;
 public class sms {
         // Find your Account SID and Auth Token at twilio.com/console
     // and set the environment variables. See http://twil.io/secure
-    public static final String ACCOUNT_SID = "AC5521533cadaf8cd209708dc445cdf91e";
-    public static final String AUTH_TOKEN = "276942d143d719305d56f8fed0ccc625";
+    public static final String ACCOUNT_SID = "";    // add in Twilio Account SID
+    public static final String AUTH_TOKEN = "";     // add in Twilio Auth Token
 
     public static void smms(String s) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         Message message = Message.creator(
-                new com.twilio.type.PhoneNumber("+18573135748"),
-                new com.twilio.type.PhoneNumber("+13346058410"),
+                new com.twilio.type.PhoneNumber(""), // To phone Number 
+                new com.twilio.type.PhoneNumber(""), //  from phone number
                 s)
             .create();
 
